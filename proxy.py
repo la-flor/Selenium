e430383eed0571beebd19ec545d03ddeeeff4763
lafBot <64456835+lafBot@users.noreply.github.com>
2021-05-24 10:45:44 -0600

    Cleaned up unnecessary code and comments

--- proxy.py
+++ proxy.py
@@ -1,6 +1,4 @@
 import logging
-import time
-from typing import get_origin
 from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 
 def get_proxy():
@@ -8,20 +6,3 @@ def get_proxy():
     proxy_list = list(req_proxy.get_proxy_list())
     local_proxy = [f'{p.ip}:{p.port}' for p in proxy_list if p.country == "United States"]
     return local_proxy
-
-    # print("Initialization took: {0} sec".format((time.time() - start)))
-    # print("Size: {0}".format(len(req_proxy.get_proxy_list())))
-    # print("ALL = {0} ".format(list(map(lambda x: x.get_address(), req_proxy.get_proxy_list()))))
-
-    test_url = 'http://ipv4.icanhazip.com'
-
-    while True:
-        start = time.time()
-        request = req_proxy.generate_proxied_request(test_url)
-        print("Proxied Request Took: {0} sec => Status: {1}".format((time.time() - start), request.__str__()))
-        if request is not None:
-            print("\t Response: ip={0}".format(u''.join(request.text).encode('utf-8')))
-        print("Proxy List Size: {0}".format(len(req_proxy.get_proxy_list())))
-
-        print("-> Going to sleep..")
-        time.sleep(10)
