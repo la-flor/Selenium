a2e892f0d2d13fb44d4a6e1d90848e6f7248def1
lafBot <64456835+lafBot@users.noreply.github.com>
2021-06-02 13:35:53 -0600

    Repaired image sourcing with WebDriverWait.  Added model_url output.

--- bc_scrape.py
+++ bc_scrape.py
@@ -3,6 +3,11 @@ import re
 from selenium.webdriver.chrome.options import Options
 import time
 
+# for WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 ####################################################################################
 ############################## Non-Proxied Scraping ################################
 ####################################################################################
@@ -30,22 +35,26 @@ categories = [
     "womens+trail+shoes"
 ]
 
+wait = WebDriverWait(driver, 10)
+
 for category in categories:
     
-  driver.get(f'https://www.backcountry.com/Store/catalog/search.jsp?s=u&q=${category}')
+  driver.get(f'https://www.backcountry.com/Store/catalog/search.jsp?s=u&q={category}')
 
   def get_products():
       products = driver.find_elements_by_class_name('product')
       for product in products:
           brand = product.find_element_by_class_name('ui-pl-name-brand').text
           model = product.find_element_by_class_name('ui-pl-name-title').text
+          model_url = product.find_element_by_class_name('ui-pl-link').get_attribute('href')
+          image = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.ui-pl-img img'))).get_attribute('src')
           try:
               price = product.find_element_by_class_name('ui-pl-pricing-low-price').text
           except:
               price = product.find_element_by_class_name('ui-pl-pricing-high-price').text
 
           clean_price = re.search("[0-9]+\.[0-9][0-9]", price).group(0)
-          print({ "category": category, "page": page, "brand": brand, "model": model, "price": clean_price})
+          print({ "category": category, "page": page, "brand": brand, "model": model, "model_url": model_url, "image": image, "price": clean_price})
 
   # BEGIN EXECUTION:
 
